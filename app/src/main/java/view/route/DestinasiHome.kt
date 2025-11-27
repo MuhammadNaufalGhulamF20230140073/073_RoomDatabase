39c9fa359c8da65e266b7fa3091b3f14194592ea
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-27 19:59:51 +0700

    Menambahkan object DestinasiHome sebagai rute utama aplikasi.

--- app/src/main/java/view/route/DestinasiHome.kt
+++
@@ -1,9 +0,0 @@
-package view.route
-
-import com.example.roomdb.R
-
-object DestinasiHome : DestinasiNavigasi {
-    override val route = "home"
-    // Diperbaiki: Menggunakan R.string.data_siswa (huruf kecil)
-    override val titleRes = R.string.data_siswa
-}
