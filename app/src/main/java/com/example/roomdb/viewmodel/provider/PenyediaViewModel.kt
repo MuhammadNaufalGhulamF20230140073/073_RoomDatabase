f810c92c4dedaf988a1dd7094a851e5772e14593
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-12-04 13:32:34 +0700

    mengubah code yang sesuai

--- app/src/main/java/com/example/roomdb/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdb/viewmodel/provider/PenyediaViewModel.kt
@@ -1,24 +1,41 @@
 package com.example.roomdb.viewmodel.provider
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+
+
 import com.example.roomdb.repositori.AplikasiSiswa
+import com.example.roomdb.viewmodel.DetailViewModel
 import com.example.roomdb.viewmodel.HomeViewModel
 import com.example.roomdb.viewmodel.EntryViewModel
 
 object PenyediaViewModel {
+
     val Factory = viewModelFactory {
+
         initializer {
-            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+            val aplikasi = this.aplikasiSiswa()
+            HomeViewModel(aplikasi.container.repositoriSiswa)
         }
 
         initializer {
-            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+            val aplikasi = this.aplikasiSiswa()
+            EntryViewModel(aplikasi.container.repositoriSiswa)
+        }
+
+        initializer {
+            val aplikasi = this.aplikasiSiswa()
+            val savedStateHandle: SavedStateHandle = this.createSavedStateHandle()
+            DetailViewModel(savedStateHandle, aplikasi.container.repositoriSiswa)
         }
     }
 }
-fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory
-        .APPLICATION_KEY] as AplikasiSiswa)
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa {
+    return this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+            as? AplikasiSiswa ?: error("Application must be AplikasiSiswa")
+}
